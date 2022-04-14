3ca31a540c4c24367df5b0f22a598f0184952da7
kevinfarukh <kevinfarukh@gmail.com>
2022-04-14 18:30:39 -0500

    public

--- src/app.js
+++ src/app.js
@@ -18,11 +18,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.use('/products', productsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,6 +1,8 @@
 const productController ={
     getProducts: (req, res) => {
-        res.render("product-detail")
+        res.render("product-detail", {
+            title: "Product Detail"
+        })
     }
 }
 
