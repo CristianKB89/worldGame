e8f565e620cee62f628ca778f2d26e7c3646161e
kevinfarukh <kevinfarukh@gmail.com>
2022-04-17 16:56:38 -0500

    signup

--- public/js/nav-bar.js
+++ public/js/nav-bar.js
@@ -4,3 +4,9 @@ const nav = document.getElementById("nav");
 menuToggle.addEventListener("click", ()=>{
     nav.classList.toggle("active")
 })
+const loginToggle = document.querySelector(".login-toggle-nav");
+const loginNav = document.getElementById("login-nav");
+
+loginToggle.addEventListener("click", ()=>{
+    loginNav.classList.toggle("active-login")
+})
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+let userController = require('../controllers/userController');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.get('/', userController.getUsers);
+router.get('/signup', userController.signup);
 module.exports = router;
