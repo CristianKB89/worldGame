91679091da424290db0b5691c2185d955c501fef
kevinfarukh <kevinfarukh@gmail.com>
2022-05-04 16:10:28 -0500

    validacion producto y comunidad

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,4 @@
+const {validationResult} = require('express-validator');
 const productsController ={
     getProducts: (req, res) => {
         res.render("product-detail", {
@@ -9,6 +10,16 @@ const productsController ={
     },
     createProduct: (req, res) => {
         res.render("product-create");
+    },
+    createProductPost: (req, res) => {
+        const result = validationResult(req);
+        
+        if(result.errors.length > 0){
+            res.render("product-create",{errors: result.mapped(), oldData: req.body});
+        }
+        else{
+            res.send("Product created");
+        }
     }
 }
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const productsController = require('../controllers/productsController');
+
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, './public/images/products')
+    },
+    filename: function(req, file, cb){
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`)
+    }
+});
+let upload = multer({storage});
+//middlewares
+
+let productValidation = require('../middlewares/createProductValidator');
+
+//rutas
 
 router.get("/", productsController.getProducts);
 router.get("/productdetail", productsController.productDetail);
+//creación de producto
 router.get ("/create", productsController.createProduct);
+router.post("/create", upload.single("imgDetail"), upload.single("imgCard"), productValidation, productsController.createProductPost);
+
 module.exports = router;
