395197a19767a6b597aa1d74c2e9a9c056f109bf
iDeerBoy <deerboy.art@gmail.com>
2022-05-12 16:22:14 -0500

    mis publicaciones

--- public/js/postUser.js
+++ public/js/postUser.js
@@ -1,12 +1,6 @@
 addEventListener('load', () => {
     const editButton = document.querySelectorAll('.editButton')
     const postEdit = document.querySelectorAll('.postEdit')
-
-    // editButton.addEventListener('click', () =>{
-    //     postEdit.classList.toggle('editOn')
-    // })
-
-    console.log(editButton, postEdit);
     
     editButton.forEach((button, i) => {
         button.addEventListener('click', () =>{
--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -3,6 +3,9 @@ addEventListener("load", () => {
   const editForm = document.getElementById("editForm");
   const editButton = document.getElementById("editButton");
   const editCancel = document.getElementById("editCancel");
+  const postButton = document.getElementById('postButton');
+  const libraryButton = document.getElementById("libraryButton");
+  const posts = document.getElementById('posts')
 
   editButton.addEventListener("click", (e) => {
     e.preventDefault;
@@ -13,4 +16,14 @@ addEventListener("load", () => {
     library.style.display = "block";
     editForm.style.display = "none";
   });
+
+  postButton.onclick = () =>{
+    library.style.display = "none";
+    posts.style.display = "block";
+  }
+
+  libraryButton.onclick = () =>{
+    library.style.display = "block";
+    posts.style.display = "none";
+  }
 });
