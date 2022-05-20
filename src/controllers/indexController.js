ca72f385eac489403de85f57e1531928f58e2a1b
iDeerBoy <deerboy.art@gmail.com>
2022-05-19 19:24:39 -0500

    arreglando logica index y catalog

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,6 +1,24 @@
+const db = require('../database/models');
+const Games = db.Game;
+
 const indexController = {
-    index: (req, res) =>{ 
-        res.render('index')
+    index: (req, res) =>{  Games.findAll()
+        .then(games => {
+            const numX = () => Math.floor(Math.random() * games.length)
+            const freeGames = games.filter(i => i.dataValues.discount === 100)
+            res.render('index', {
+                game1: games[numX()].dataValues,
+                game2: games[numX()].dataValues,
+                game3: games[numX()].dataValues,
+                game4: games[numX()].dataValues,
+                game5: games[numX()].dataValues,
+                game6: games[numX()].dataValues,
+                game7: games[numX()].dataValues,
+                game8: games[numX()].dataValues,
+                freeGame1: freeGames[0],
+                freeGame2: freeGames[1]
+            })
+        })
     },
     /* search: (req, res) =>{
         const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
