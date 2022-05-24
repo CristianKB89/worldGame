a1a0f3c32a2989d5b1444b21705b17938c72f5ba
CristianKB89 <cristianiksl@gmail.com>
2022-05-24 10:57:22 -0500

    Variables de entorno

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,27 @@
-module.exports={
+require('dotenv').config();
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "",
-    "database": "dbworldgame",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": '',
     "database": "dbworldgame",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "dbworldgame",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   }
 }
+
